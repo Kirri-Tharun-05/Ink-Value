1d7c486cd51ed2b9fdc54b652c36ba399ba80c23
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-03-28 19:17:52 +0530

    Updated Environment.js file for Frontend Url

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,4 +1,4 @@
-let ON_PROD=false;
-const server= ON_PROD? 'https://video-conference-application-backend.onrender.com':'http://localhost:8080' ;
+let ON_PROD=true;
+const server= ON_PROD? 'https://ink-value-backend.onrender.com':'http://localhost:8080' ;
 
 export default server;
